368a3d50e9e4eb502df443c536b5760b04470f20
BRIAN BIXBY <BRIANBIXBY0@GMAIL.COM>
2022-07-16 05:52:01 -0700

    updated content security policy

--- server.js
+++ server.js
@@ -1,12 +1,49 @@
 'use strict';
 
+require('dotenv').config();
 const compression = require('compression');
 const express = require('express');
 const app = express();
+const helmet = require('helmet');
 const PORT = process.env.PORT || 3000;
 const path = require('path');
 
 app.use(compression());
+
+app.use(
+  helmet.contentSecurityPolicy({
+    useDefaults: false,
+    directives: {
+      'default-src': "'self'",
+      'script-src': [
+        "'self'",
+        'https://fonts.gstatic.com',
+        'https://fonts.googleapis.com',
+      ],
+      'style-src': [
+        "'self'",
+        'https://fonts.gstatic.com',
+        'https://fonts.googleapis.com',
+      ],
+      'connect-src': [
+        "'self'",
+        process.env.REACT_APP_API_URL,
+        'https://api.edamam.com',
+      ],
+      'font-src': [
+        "'self'",
+        'https://fonts.gstatic.com',
+        'https://fonts.googleapis.com',
+      ],
+      'img-src': [
+        "'self'",
+        'https://api.edamam.com',
+        'https://edamam-product-images.s3.amazonaws.com',
+      ],
+    },
+  })
+);
+
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', function (req, res) {
